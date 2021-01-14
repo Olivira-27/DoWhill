a03508135a0500f9027941950ea473eaafba6657
Olivira-27 <oliviraadi@gmail.com>
2021-01-14 19:16:00 +0700

    latihan 1

---
+++ src/pkgdo/whill/DoWhill1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgdo.whill;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DoWhill1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        int bil = 1 ;
+        do {
+            //statement
+              System.out.print(bil+ " ");
+              bil++;     
+              }while (bil<=10);
+        
+    }
+    
+}
